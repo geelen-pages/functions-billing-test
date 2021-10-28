eacee837a65bb0481099a8df62c9c867c21f773f
geelen-pages <93134883+geelen-pages@users.noreply.github.com>
2021-10-28 13:28:34 +0100

    Update api-worker.mjs

--- api-worker.mjs
+++ api-worker.mjs
@@ -1,18 +1,25 @@
 export default {
-  async fetch(request) {
+  async fetch(request, env) {
     const { url, method } = request
     const headers = {}
 
     for (const [k, v] of request.headers.entries()) {
       headers[k] = v
     }
+    
+    const subrequest = await env.ASSETS.fetch(request)
 
     const payload = {
       headers,
       url,
       method,
       body: await request.text(),
-      cf: request.cf
+      cf: request.cf,
+      subrequest: {
+        status: subrequest.statusCode,
+        headers: subrequest.headers,
+        body: await subrequest.text()
+      }
     }
     console.log(payload)
     return new Response(JSON.stringify(payload), {
